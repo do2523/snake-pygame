e40bfd7e3a7cfab744daf54d62bf9a3db428af6a
do262431@gmail.com <do262431@gmail.com>
2024-02-24 14:10:20 -0500

    Agent update

--- agent.py
+++ agent.py
@@ -26,14 +26,35 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, reward, next_state, done):
         pass
 
     def get_action(self, state):
         pass
 
 def train():
-    plot_scores =  
+    plot_scores =  []
+    plot_mean_scores = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGameAI()
+    while True:
+        # get old state
+        state_old = agent.get_state(game)
 
+        # get move 
+        final_move = agent.get_action(state_old)
+
+        # perform move and get new state
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # train short memory 
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+ 
 if __name__ == '__main__':
     train()
